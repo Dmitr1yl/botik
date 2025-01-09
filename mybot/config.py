569c9619d292f1016ce612db96dd4968c249eaa9
Dmitr1yl <Lew.dmitry@icloud.com>
2025-01-09 19:30:35 +0300

    config.py

--- mybot/config.py
+++ mybot/config.py
@@ -1,6 +1,5 @@
 # config.py
-BOT_TOKEN = "7957884864:AAEGi-rTgee-hXXqMl6ba15CfEXSvUxht3c"
-ADMIN_ID = "764427725"
-DIALOGFLOW_CREDENTIALS = "small-talk-qpp9-01bbaad3cc34.json"  # Укажите путь к файлу JSON
-PROJECT_ID = "small-talk-qpp9"  # Ваш ID проекта Dialogflow
-
+BOT_TOKEN = "токен"
+ADMIN_ID = "айди тг"
+DIALOGFLOW_CREDENTIALS = "путь к файлу JSON"  
+PROJECT_ID = "ID проекта Dialogflow"  
